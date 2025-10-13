4fa801363442c716ad35b16404bae8d7c4c92731
mazenkhaled98 <mazenkhaled8@gmail.com>
2025-10-13 18:30:33 +0300

    video4

---
+++ Core/Domain/Entites/ProductModule/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entites.ProductModule
+{
+    public class BaseEntity <TKey>
+    {
+
+        public TKey Id { get; set; }
+    }
+}
---
+++ Core/Domain/Entites/ProductModule/ProductBrand.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Entites.ProductModule
+{
+    public class ProductBrand :BaseEntity<int>
+    {
+        public string Name { get; set; } = null!;
+    }
+}
